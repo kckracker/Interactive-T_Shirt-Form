14fb002bff146c0058b16800d2b38c6362972195
Casey Holland <caseyholland972@gmail.com>
2020-11-28 13:27:49 -0500

    Add Event Listeners
    Adding event listeners to display other job role on "other" selection and shirt colors matching selection upon selection of shirt option.

--- js/script.js
+++ js/script.js
@@ -4,4 +4,27 @@ window.onload = function nameFocus() {
     document.querySelector('#other-job-role').style.display = 'none';
     document.querySelector('#shirt-colors').style.display = 'none';
 }
+// Setting an event listener on the Select list
+document.querySelector('#title').addEventListener('change', (e) => {
+    // Conditional statement to check if selection value equals "other"
+    if (e.target.value === "other"){
+        // Displaying other job role input upon Other selection, else hiding from view
+    document.querySelector('#other-job-role').style.display = 'block';
+    } else {
+        document.querySelector('#other-job-role').style.display = 'none';    
+    }
+});
 
+// Creating event listener on Shirt Designs element
+document.querySelector('#shirt-designs').addEventListener('change', (e) => {
+    document.querySelector('#shirt-colors').style.display = 'block';
+    const colors = document.querySelector('#color');
+    const shirts = document.querySelector('#design');
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i].getAttribute('data-theme') !==  e.target.value ) {
+            colors[i].hidden = true;
+        } else {
+            colors[i].hidden = false;
+        }
+    }
+});
