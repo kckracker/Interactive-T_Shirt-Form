99c77540594fb9f916a6a97872ef64a3b939578a
Casey Holland <caseyholland972@gmail.com>
2020-11-28 13:26:41 -0500

    Hiding elements on page load
    Hiding 'other job role' box, 'shirt color' options, and focusing on 'name' box upon page load via window.onload method.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+// Generating function to focus on name input, hide other roles, and hide shirt colors upon loading of page
+window.onload = function nameFocus() {
+    document.querySelector('#name').focus();
+    document.querySelector('#other-job-role').style.display = 'none';
+    document.querySelector('#shirt-colors').style.display = 'none';
+}
 
